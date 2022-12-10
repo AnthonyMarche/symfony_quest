5cc794ce9f9ed4ddaa137123560e2e22ab2f1f54
AnthonyMarche <marche.anthony72@gmail.com>
2022-12-10 18:40:53 +0100

    quest16 done

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -35,7 +35,7 @@ class CategoryController extends AbstractController
 
         $form->handleRequest($request);
 
-        if ($form->isSubmitted()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             $categoryRepository->save($category, true);
 
             // Redirect to categories list
